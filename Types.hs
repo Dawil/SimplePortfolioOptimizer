e2d4d2a842ce448188cce90a9909cd6545c8dfff
dave <dave@davidgwilcox.com>
2013-10-28 15:22:04 +1100

    created Types file

---
+++ Types.hs
@@ -0,0 +1,35 @@
+module Types
+where
+
+import  qualified  Data.Text   as  T
+import  qualified  Data.Dates  as  D
+
+type Symbol = T.Text
+
+data Quote = Quote
+  {  date     ::  D.DateTime
+  ,  open     ::  Double
+  ,  high     ::  Double
+  ,  low      ::  Double
+  ,  close    ::  Double
+  ,  volumne  ::  Double
+  } deriving (Show)
+
+data HistoricalData = HistoricalData
+  { symbol :: Symbol
+  , quotes :: [Quote]
+  } deriving (Show)
+
+data Portfolio = Portfolio
+  { allocations :: [(HistoricalData,Int)]
+  } deriving (Show)
+
+data Assessment = Assessment
+  { stddev :: Double
+  , average :: Double
+  , sharpeRatio :: Double
+  , cumulativeReturns :: Double
+  } deriving (Show, Eq)
+
+instance Ord Assessment where
+  compare a1 a2 = compare (sharpeRatio a1) (sharpeRatio a2)
