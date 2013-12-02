446a1d662b96b0168209ebbfc27e131999de0821
dave <dave@davidgwilcox.com>
2013-12-02 14:23:42 +1100

    fixes #1. Main method can load a symbol's quotes.

--- src/Loader.hs
+++ src/Loader.hs
@@ -1,4 +1,27 @@
 module Loader where
 
+import Network.HTTP.Base
+import Network.HTTP
+
+
 -- This is the module where the functions for loading quotes from the Yahoo API
 -- are.
+type Symbol = String
+type Date = String
+type URL = String
+
+getRsp query = simpleHTTP $ getRequest query
+parseBody :: String -> Either String Query
+parseBody body = A.eitherDecode $ B.pack body
+
+constructQuotesQuery :: [Symbol] -> Date -> Date -> URL
+constructQuotesQuery symbols startDate endDate =
+  "http://query.yahooapis.com/v1/public/yql?q=" ++ yqlString ++ "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+  where
+    yqlString = --urlEncode $
+      "select%20*" ++ (urlEncode $
+      " from yahoo.finance.historicaldata\
+      \ where symbol in (" ++ commaDelimited (map show symbols) ++ ")\
+      \ and startDate = " ++ show startDate ++ " and endDate = " ++ show endDate)
+    -- Fails on an empty list
+    commaDelimited (x:xs) = foldr (\a b->a ++ "," ++ b) x xs
--- src/Main.hs
+++ src/Main.hs
@@ -5,22 +5,18 @@ import Data.Char (toUpper)
 
 import Types
 import Quotes
+import Loader
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy.Char8 as B
 
-import Network.HTTP
-
-getRsp = simpleHTTP $ getRequest "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22ACB.AX%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-getBody = getRsp >>= getResponseBody
-parseBody :: String -> Either String Query
-parseBody body = A.eitherDecode $ B.pack body
 
 main :: IO ()
 main = do
-  symbols <- fmap (map (map toUpper) )getArgs
-  print symbols
-  datum <- jsonIO 
-  let theQuote = case A.decode datum of
-                   Just q -> return q :: IO Quote
-                   _ -> undefined
-  theQuote >>= print 
+  symbols <- fmap (map (map toUpper) ) getArgs
+  putStr $ "Fetching quotes for " ++ show symbols ++ "..."
+  rsp <- getRsp $ constructQuotesQuery symbols "2009-09-11" "2010-03-10"
+  putStrLn "Done."
+  body <- getResponseBody rsp >>= return . parseBody
+  case body of
+    Right query -> print . head $ results query
+    Left err -> putStrLn $ "Error: " ++ err
